6eaba124d8b1a6dd81f5343ebaf9705abe210442
jumelet <jumeletjaap@gmail.com>
2019-01-06 13:08:37 +0100

    Add extractor docstrings + large refactor

--- lm_diagnoser/extractors/init_extractor.py
+++ lm_diagnoser/extractors/init_extractor.py
@@ -1,147 +1,141 @@
-from typing import BinaryIO, Dict, List, Type, Tuple
+from typing import BinaryIO, Dict, List, Optional, Tuple
 
 import torch
 import pickle
 from time import time
 from contextlib import ExitStack
-from pathlib import Path
-from models.language_model import ActivationDict, LanguageModel
+from models.language_model import LanguageModel
+from corpus.labeled import LabeledCorpus, LabeledSentence
+from embeddings.initial import InitEmbs
 
 
 ActivationName = Tuple[int, str]
-ActivationNames = List[ActivationName]
 ActivationFiles = Dict[ActivationName, BinaryIO]
-Sentence = List[str]
 
 
 class Extractor:
     """ Extracts all intermediate activations of a LM from a corpus.
 
-    Only activations that are provided in extraction_names will be
+    Only activations that are provided in activation_names will be
     stored in a pickle file. Each activation is written to its own file.
 
-    Args:
-        model: Language model that inherits from AbstractLM
-        corpus_path: Location of labeled corpus.
-        extraction_names: List of activations to be extracted of the form
-            [(l, n)] with
-                l (int): the layer number.
-                n (str): activation name as defined in model.
-        init_embs_path (optional): Location of initial embeddings.
+    Parameters
+    ----------
+    model : LanguageModel
+        Language model that inherits from LanguageModel.
+    corpus_path : str
+        Location of labeled corpus.
+    activation_names : List[ActivationName]
+        List of activations to be stored of the form [(l, n)] with
+            l (int): the layer number.
+            n (str): activation name as defined in model.
+    init_embs_path : str, optional (default = '')
+        Location of initial embeddings.
+
+    Attributes
+    ----------
+    model : LanguageModel
+        Language model that inherits from LanguageModel.
+    hidden_size : int
+        Number of hidden units in model.
+    activation_names : List[ActivationName]
+        List of activations to be stored.
+    activation_files : ActivationFiles
+        Dict of files to which activations will be written.
+    label_file: Optional[BinaryIO]
+        File to which sentence labels will be written.
+    corpus : LabeledCorpus
+        corpus containing the labels for each sentence.
+    num_extracted : int
+        Total number of extracted activations (1 per token)
     """
     def __init__(self,
-                 model: Type[LanguageModel],
+                 model: LanguageModel,
                  corpus_path: str,
-                 extraction_names: ActivationNames,
+                 activation_names: List[ActivationName],
                  init_embs_path: str = '') -> None:
+
         self.model = model
         self.hidden_size = model.hidden_size
-        self.extraction_names = extraction_names
+        self.activation_names = activation_names
+        self.activation_files: ActivationFiles = {}
+        self.label_file: Optional[BinaryIO] = None
+        self.num_extracted: int = 0
 
         with open(corpus_path, 'rb') as f:
-            self.corpus = pickle.load(f)
+            self.corpus: LabeledCorpus = pickle.load(f)
 
-        self.init_embs = self.create_init_embs(init_embs_path)
+        self.init_embs = InitEmbs(init_embs_path, model).activations
 
-    def create_init_embs(self, init_embs_path: str) -> ActivationDict:
-        """
-        Create initial embeddings from path or create zero initialized
-        embeddings.
-
-        :param init_embs_path: location of init embeddings
-        :return: init_embs: ActivationDict with init embeddings
-        """
-        if init_embs_path:
-            assert Path(init_embs_path).is_file(), 'File does not exist'
-
-            with open(init_embs_path, 'rb') as f:
-                init_embs = pickle.load(f)
-
-            assert len(init_embs) == self.model.num_layers, \
-                'Number of initial layers not correct'
-            assert all(
-                    'hx' in a.keys() and 'cx' in a.keys()
-                    for a in init_embs.values()
-                ), 'Initial layer names not correct, should be hx and cx'
-            assert len(init_embs[0]['hx']) == self.hidden_size, \
-                'Initial activation size is incorrect'
-
-            return init_embs
-
-        return {
-            l: {
-                'hx': torch.zeros(self.hidden_size),
-                'cx': torch.zeros(self.hidden_size)
-            } for l in range(self.model.num_layers)
-        }
-
+    # TODO: Allow batch input
     def extract(self,
                 output_path: str,
-                cutoff: int = 32,
-                print_every: int = 10) -> None:
+                cutoff: int = -1,
+                print_every: int = 100) -> None:
+        """ Extracts embeddings from a labeled corpus.
 
+        Uses an ExitStack to write to multiple files at once.
+
+        Parameters
+        ----------
+        output_path : str
+            Directory to which extractions will be saved.
+        cutoff : int, optional (default = -1)
+            Number of sentences to be extracted. Set to -1 to extract
+            entire corpus.
+        print_every : int, optional (default = 100)
+            Print progress info every n steps.
+        """
         start_time: float = time()
 
         with ExitStack() as stack:
-            files: ActivationFiles = {
-                (l, name):
-                    stack.enter_context(
-                        open(f'{output_path}/{name}_l{l}.pickle', 'wb')
-                    )
-                for (l, name) in self.extraction_names
-            }
-            labels: BinaryIO = stack.enter_context(
-                open(f'{output_path}/labels.pickle', 'wb')
-            )
+            self.create_output_files(output_path, stack)
 
-            tot_n = self.extract_corpus(files, labels, start_time, cutoff, print_every)
+            for n, labeled_sentence in enumerate(self.corpus):
+                if n % print_every == 0:
+                    print(f'{n}\t{time() - start_time:.2f}s')
+
+                self.extract_sentence(labeled_sentence)
+
+                self.num_extracted += len(labeled_sentence.sen)
+
+                if cutoff == n:
+                    break
 
         n_sens = len(self.corpus) if cutoff == -1 else cutoff
         print(f'\nExtraction finished.')
-        print(f'{n_sens} sentences have been extracted, yielding {tot_n} data points.')
+        print(f'{n_sens} sentences have been extracted, yielding {self.num_extracted} data points.')
         print(f'Total time took {time() - start_time:.2f}s')
 
-    def extract_corpus(self,
-                       files: ActivationFiles,
-                       labels: BinaryIO,
-                       start_time: float,
-                       cutoff: int,
-                       print_every: int) -> int:
+    def create_output_files(self, output_path: str, stack: ExitStack) -> None:
+        self.activation_files = {
+            (l, name):
+                stack.enter_context(
+                    open(f'{output_path}/{name}_l{l}.pickle', 'wb')
+                )
+            for (l, name) in self.activation_names
+        }
+        self.label_file = stack.enter_context(
+            open(f'{output_path}/labels.pickle', 'wb')
+        )
 
-        tot_n = 0
-
-        for n, sentence in enumerate(self.corpus.values()):
-            if n % print_every == 0:
-                print(f'{n}\t{time() - start_time:.2f}s')
-
-            self.extract_sentence(sentence, files, labels)
-
-            tot_n += len(sentence.sen)
-
-            if cutoff == n:
-                break
-
-        return tot_n
-
-    def extract_sentence(self,
-                         sentence: Sentence,
-                         files: ActivationFiles,
-                         labels: BinaryIO) -> None:
+    def extract_sentence(self, labeled_sentence: LabeledSentence) -> None:
+        labeled_sentence.validate()
 
         sen_activations = {
-            (l, name): torch.zeros(len(sentence.sen), self.hidden_size)
-            for (l, name) in self.extraction_names
+            (l, name): torch.zeros(len(labeled_sentence), self.hidden_size)
+            for (l, name) in self.activation_names
         }
 
         activations = self.init_embs
 
-        for i, token in enumerate(sentence.sen):
+        for i, token in enumerate(labeled_sentence.sen):
             out, activations = self.model(token, activations)
 
-            for l, name in self.extraction_names:
+            for l, name in self.activation_names:
                 sen_activations[(l, name)][i] = activations[l][name]
 
-        for l, name in self.extraction_names:
-            pickle.dump(sen_activations[(l, name)], files[(l, name)])
+        for l, name in self.activation_names:
+            pickle.dump(sen_activations[(l, name)], self.activation_files[(l, name)])
 
-        pickle.dump(sentence.labels, labels)
+        pickle.dump(sentence.labels, self.label_file)
