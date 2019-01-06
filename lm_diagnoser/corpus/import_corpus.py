e2955b38f94021f6122b655e1611de55721d13ec
jumelet <jumeletjaap@gmail.com>
2019-01-06 16:18:53 +0100

    Import and convert labeled corpus from list

---
+++ lm_diagnoser/corpus/import_corpus.py
@@ -0,0 +1,22 @@
+from typing import Any, Dict, List
+
+from customtypes.corpus import LabeledCorpus, LabeledSentence
+import pickle
+
+
+def convert_to_labeled_corpus(corpus_path: str) -> LabeledCorpus:
+    labeled_corpus = []
+
+    with open(corpus_path, 'rb') as f:
+        corpus: List[Dict[str, Any]] = pickle.load(f)
+
+    for item in corpus:
+        assert 'sen' in item.keys() and 'labels' in item.keys(), 'Corpus item has wrong format.'
+
+        sen = item['sen']
+        labels = item['labels']
+        misc_info = {k: v for k, v in item.items() if k not in ['sen', 'labels']}
+        labeled_sentence = LabeledSentence(sen, labels, misc_info)
+        labeled_corpus.append(labeled_sentence)
+
+    return labeled_corpus
