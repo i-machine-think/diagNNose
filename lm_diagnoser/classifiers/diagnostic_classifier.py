9397ce9f6d10e2869c87c29f0de2b72c33ecb2c1
jumelet <jumeletjaap@gmail.com>
2019-01-28 00:59:49 +0100

    Big classifier refactor

--- lm_diagnoser/classifiers/diagnostic_classifier.py
+++ lm_diagnoser/classifiers/diagnostic_classifier.py
@@ -1,7 +1,7 @@
 import pickle
 from collections import defaultdict
 from time import time
-from typing import Any, Dict, List
+from typing import Any, List
 
 import numpy as np
 from sklearn.externals import joblib
@@ -9,6 +9,8 @@ from sklearn.linear_model import LogisticRegressionCV as LogReg
 from sklearn.metrics import accuracy_score
 
 from ..typedefs.models import ActivationName
+from ..typedefs.classifiers import ResultsDict
+from ..activations.activations_reader import ActivationsReader
 
 
 class DiagnosticClassifier:
@@ -16,103 +18,82 @@ class DiagnosticClassifier:
                  activations_dir: str,
                  activation_names: List[ActivationName],
                  output_dir: str,
+                 classifier: str,
                  labels: str = 'labels.pickle',
                  keys: str = 'keys.pickle') -> None:
 
-        self.activations_dir = activations_dir
         self.activation_names: List[ActivationName] = activation_names
         self.output_dir = output_dir
+        self.classifier = classifier
 
-        self.keys = self._read_keys(keys)
-        self.labels = self._read_labels(labels)
-        self.data_len = len(self.labels)
+        self.activations_reader = ActivationsReader(activations_dir, labels, keys)
+        self.model: Any = None
+        self.results: ResultsDict = defaultdict(dict)
 
-    # TODO: Write json file to ouput path containing experiment information
     def classify(self, subset_size: int = -1) -> None:
+        start_t = time()
+
+        for a_name in self.activation_names:
+            self._create_classifier()
+            data_dict = self.activations_reader.create_data_split(a_name, subset_size)
+
+            self.fit_data(data_dict['train_x'], data_dict['train_y'], a_name)
+            pred_y = self.eval_model(data_dict['test_x'], data_dict['test_y'], a_name)
+
+            self.save_model(pred_y, a_name)
+
+        self.log_results(start_t)
+
+    def _create_classifier(self) -> None:
+        self.model = {
+            'logreg': LogReg(),
+            'svm': None,
+        }[self.classifier]
+
+    def fit_data(self,
+                 train_x: np.array, train_y: np.array,
+                 activation_name: ActivationName) -> None:
+        print(f'\nStarting fitting model on {activation_name}...')
 
         start_time = time()
+        self.model.fit(train_x, train_y)
 
-        results: Dict[ActivationName, List[float]] = defaultdict(list)
+        print(f'Fitting done in {time() - start_time:.2f}s')
 
-        for activation_name in self.activation_names:
-            l, name = activation_name
-            activations = self._read_activations(activation_name)
+    # TODO: Add more evaluation metrics here
+    def eval_model(self,
+                   test_x: np.array, test_y: np.array,
+                   activation_name: ActivationName) -> np.ndarray:
+        pred_y = self.model.predict(test_x)
 
-            data_dict = self._split_data(activations, subset_size=subset_size)
+        acc = accuracy_score(test_y, pred_y)
 
-            model = LogReg()
+        print(f'{activation_name} acc.:', acc)
 
-            print(f'\nStarting fitting model on {activation_name}...')
-            t0 = time()
+        self.results[activation_name]['acc'] = acc
 
-            model.fit(data_dict['train_x'], data_dict['train_y'])
+        return pred_y
 
-            print(f'Fitting done in {time() - t0:.2f}s')
-            y_pred = model.predict(data_dict['test_x'])
-            acc = accuracy_score(data_dict['test_y'], y_pred)
-            print(f'{activation_name} acc.:', acc)
-            results[activation_name].append(acc)
-
-            with open(f'{self.output_dir}/preds/{name}_l{l}.pickle', 'wb') as file:
-                pickle.dump(y_pred, file)
-            joblib.dump(model, f'{self.output_dir}/models/{name}_l{l}.pickle')
-
-        print(f'Total classification time took {time() - start_time:.2f}s')
-
-        for k, v in results.items():
-            print(f'{k} {np.mean(v):.4f}+/-{np.std(v):.6f}')
-
-    def _read_keys(self, keys_name: str) -> np.array:
-        with open(f'{self.activations_dir}/{keys_name}', 'rb') as f:
-            keys = pickle.load(f)
-
-        return keys
-
-    def _read_labels(self, label_name: str) -> np.array:
-        with open(f'{self.activations_dir}/{label_name}', 'rb') as f:
-            labels = pickle.load(f)
-
-        return labels
-
-    def _read_activations(self, activation_name: ActivationName) -> np.array:
+    def save_model(self, pred_y: np.ndarray, activation_name: ActivationName) -> None:
         l, name = activation_name
-        filename = f'{name}_l{l}.pickle'
 
-        hidden_size = None
-        activations = None
+        with open(f'{self.output_dir}/preds/{name}_l{l}.pickle', 'wb') as file:
+            pickle.dump(pred_y, file)
 
-        n = 0
+        joblib.dump(self.model, f'{self.output_dir}/models/{name}_l{l}.pickle')
 
-        with open(f'{self.activations_dir}/{filename}', 'rb') as f:
-            while True:
-                try:
-                    sen_activations = pickle.load(f)
+    def log_results(self, start_t: float) -> None:
+        total_time = time() - start_t
+        print(f'Total classification time took {total_time:.2f}s')
 
-                    if hidden_size is None:
-                        hidden_size = sen_activations.shape[1]
-                        activations = np.zeros((self.data_len, hidden_size))
-
-                    i = len(sen_activations)
-                    activations[n:n+i] = sen_activations
-                    n += i
-                except EOFError:
-                    break
-
-        return activations
-
-    def _split_data(self,
-                    activations: np.array,
-                    subset_size: int = -1,
-                    train_split: float = 0.9) -> Any:
-
-        split = int(self.data_len * train_split)
-
-        n = split if subset_size == -1 else subset_size
-        train_indices = np.random.choice(range(n), n, replace=False)
-
-        return {
-            'train_x': activations[train_indices],
-            'train_y': self.labels[train_indices],
-            'test_x': activations[split:],
-            'test_y': self.labels[split:]
+        log = {
+            'activation_names': self.activation_names,
+            'classifier': self.classifier,
+            'results': self.results,
+            'total_time': total_time,
         }
+
+        with open(f'{self.output_dir}/log.pickle', 'wb') as file:
+            pickle.dump(log, file)
+
+
