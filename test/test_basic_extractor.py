78db8330ca3237fb880b70559841a36e918d0931
Kaleidophon <Dennis.Ulmer@gmx.de>
2019-02-06 19:07:49 +0100

    Adding TODOs and some boilerplate code.

---
+++ test/test_basic_extractor.py
@@ -0,0 +1,12 @@
+"""
+Test the code in rnnalayse.extractors.base_extractor.py.
+"""
+
+import unittest
+
+
+class TestExtractor(unittest.TestCase):
+    # TODO: Test selection function
+    # TODO: Test extraction itself
+    # TODO: Test calculation of average eos activations
+    ...
---
+++ test/test_dc_trainer.py
@@ -0,0 +1,11 @@
+"""
+Test the code in rnnalayse.classifiers.dc_trainer.py.
+"""
+
+import unittest
+
+
+class TestDCTrainer(unittest.TestCase):
+    # TODO: Test if class weights are accurately calculated
+    # TODO: Test if class weights are used if flag is given
+    ...
