3f20d774f8c4b2b6ae87af6cce88c529cb4c6d82
jumelet <jumeletjaap@gmail.com>
2019-02-14 19:44:20 +0100

    :books: Add ActivationReader documentation

--- rnnalyse/activations/activation_reader.py
+++ rnnalyse/activations/activation_reader.py
@@ -9,6 +9,24 @@ from ..utils.paths import load_pickle, trim
 
 
 class ActivationReader:
+    """ Reads in pickled activations that have been extracted.
+
+    Parameters
+    ----------
+    activations_dir : str
+        Directory containing the extracted activations
+    label_path : str, optional
+        Path to pickle file containing the labels. Defaults to
+        labels.pickle in activations_dir if no path has been provided.
+
+    Attributes
+    ----------
+    activations_dir : str
+    labels : np.ndarray
+        Numpy array containing the extracted labels
+    data_len : int
+        Number of extracted activations
+    """
     def __init__(self,
                  activations_dir: str,
                  label_path: Optional[str] = None) -> None:
@@ -18,13 +36,25 @@ class ActivationReader:
         self.labels = self._read_labels(label_path)
         self.data_len = len(self.labels)
 
-    def _read_labels(self, label_path: Optional[str]) -> np.array:
+    def _read_labels(self, label_path: Optional[str]) -> np.ndarray:
         if label_path is None:
             label_path = f'{self.activations_dir}/labels.pickle'
 
         return load_pickle(label_path)
 
-    def read_activations(self, activation_name: ActivationName) -> np.array:
+    def read_activations(self, activation_name: ActivationName) -> np.ndarray:
+        """ Reads the pickled activations of activation_name
+        
+        Parameters
+        ----------
+        activation_name : ActivationName
+            (layer, name) tuple indicating the to-be-read-in activations
+        
+        Returns
+        -------
+        activations : np.ndarray
+            Numpy array of activation values
+        """
         l, name = activation_name
         filename = f'{name}_l{l}.pickle'
 
@@ -40,7 +70,7 @@ class ActivationReader:
                 try:
                     sen_activations = pickle.load(f)
 
-                    # To make hidden size dependent of data only the activations array
+                    # To make hidden size dependent of data only, the activations array
                     # is created only after observing the first batch of activations.
                     if hidden_size is None:
                         hidden_size = sen_activations.shape[1]
@@ -58,6 +88,18 @@ class ActivationReader:
                           activation_name: ActivationName,
                           data_subset_size: int = -1,
                           train_test_split: float = 0.9) -> DataDict:
+        """ Creates train/test data split of activations
+
+        Parameters
+        ----------
+        activation_name : ActivationName
+            (layer, name) tuple indicating the to-be-read-in activations
+        data_subset_size : int, optional
+            Subset size of data to train on. Defaults to -1, indicating
+            the entire data set.
+        train_test_split : float
+            Percentage of the train/test split. Defaults to 0.9.
+        """
 
         if data_subset_size != -1:
             assert 0 < data_subset_size <= self.data_len, \
