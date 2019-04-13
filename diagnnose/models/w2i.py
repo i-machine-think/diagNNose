3dcd839b7e3f7582e17513d81b805d9203a0b6a0
jumelet <jumeletjaap@gmail.com>
2019-04-13 15:47:05 +0200

    :bug: Add unk token if not present in w2i

--- diagnnose/models/w2i.py
+++ diagnnose/models/w2i.py
@@ -1,4 +1,5 @@
 from typing import Any, Dict
+from warnings import warn
 
 
 class W2I(dict):
@@ -8,8 +9,13 @@ class W2I(dict):
     be present in the vocab file.
     """
     def __init__(self, w2i: Dict[str, int], unk_token: str = '<unk>') -> None:
+        if unk_token not in w2i:
+            warn('Unk token not present, will be appended to dictionary')
+            w2i[unk_token] = len(w2i)
+
         self.unk_idx = w2i[unk_token]
         super().__init__(w2i)
 
     def __missing__(self, key: Any) -> Any:
         return self.unk_idx
+
