77dba6422ddc03396f9f583bc023689cfbd5d619
jumelet <jumeletjaap@gmail.com>
2019-05-13 18:00:07 +0200

    :recycle: Update example scripts

--- example_dir/decompose.py
+++ example_dir/decompose.py
@@ -18,42 +18,61 @@ def init_argparser() -> ArgumentParser:
     # Required args are not set to be required here as they can come from --config
     from_cmd = parser.add_argument_group('From commandline',
                                          'Specify experiment setup via commandline arguments')
-    from_cmd.add_argument('--activations_dir',
-                          help='Path to folder containing activations to decompose.')
-    from_cmd.add_argument('--decoder',
-                          help='Path to decoder classifier.')
-    from_cmd.add_argument('--decomposer',
-                          help='Class name of decomposer constructor. As of now either '
-                               'CellDecomposer or ContextualDecomposer')
+
+    # Gulordava ForwardLSTM args
     from_cmd.add_argument('--model_path',
                           help='Path to model parameters')
     from_cmd.add_argument('--vocab_path',
                           help='Path to model vocabulary')
     from_cmd.add_argument('--module_path',
                           help='Path to folder containing model module')
+    from_cmd.add_argument('--device',
+                          help='(optional) Torch device name on which model will be run.'
+                               'Defaults to cpu.')
+
+    # GoogleLM args
+    from_cmd.add_argument('--pbtxt_path',
+                          help='Path to the .pbtxt file containing the GraphDef model setup.')
+    from_cmd.add_argument('--ckpt_dir',
+                          help='Path to folder containing parameter checkpoint files.')
+    from_cmd.add_argument('--corpus_vocab_path',
+                          help='Path to the corpus vocabulary. This allows for only a subset of the'
+                               'model softmax to be loaded in.')
+    from_cmd.add_argument('--full_vocab_path',
+                          help='Path to the full model vocabulary of 800k tokens.')
+
+    # Extracted activation args
+    from_cmd.add_argument('--activations_dir',
+                          help='Path to folder containing activations to decompose.')
     from_cmd.add_argument('--init_lstm_states_path',
                           help='(optional) Location of initial lstm states of the model. '
                                'If no path is provided zero-initialized states will be used at the'
                                'start of each sequence.')
 
+    # Decomposition args
+    from_cmd.add_argument('--decomposer',
+                          help='Class name of decomposer constructor. As of now either '
+                               'CellDecomposer or ContextualDecomposer')
+
     return parser
 
 
 if __name__ == '__main__':
-    required_args = {'model_type', 'model_path', 'vocab_path', 'module_path', 'activations_dir'}
+    required_args = {'model_type', 'activations_dir'}
     arg_groups = {
         'decompose': {'decomposer', 'activations_dir', 'num_layers', 'hidden_size',
                       'init_lstm_states_path'},
-        'decoder': {'model_type', 'model_path', 'vocab_path', 'module_path'},
+        'model': {'model_type', 'model_path', 'vocab_path', 'module_path', 'pbtxt_path', 'ckpt_dir',
+                  'corpus_vocab_path', 'full_vocab_path', 'device'},
     }
     argparser = init_argparser()
 
     config_object = ConfigSetup(argparser, required_args, arg_groups)
     config_dict = config_object.config_dict
 
-    model = import_model_from_json(config_dict['decoder'])
+    model = import_model_from_json(config_dict['model'])
 
     constructor = DecomposerFactory(model, **config_dict['decompose'])
-    decomposer = constructor.create(64, slice(0, 6, 1), classes=[model.w2i['is'], model.w2i['are']])
-    cd = decomposer.decompose(0, 1, ['rel-rel', 'rel-b'])
+    decomposer = constructor.create(0, slice(0, 6, 1), classes=[model.vocab['the']])
+    cd = decomposer.decompose(1, 2, ['rel-rel', 'rel-b'])
     print(cd['relevant'], cd['irrelevant'])
--- example_dir/extract.py
+++ example_dir/extract.py
@@ -8,6 +8,7 @@ from diagnnose.models.language_model import LanguageModel
 from diagnnose.typedefs.corpus import Corpus
 
 
+# TODO: move this to a general function in /config, as it's mostly duplicated in the other scripts.
 def init_argparser() -> ArgumentParser:
     parser = ArgumentParser()
 
@@ -21,19 +22,32 @@ def init_argparser() -> ArgumentParser:
     # Required args are not set to be required here as they can come from --config
     from_cmd = parser.add_argument_group('From commandline',
                                          'Specify experiment setup via commandline arguments')
+
+    # Gulordava ForwardLSTM args
     from_cmd.add_argument('--model_path',
                           help='Path to model parameters')
     from_cmd.add_argument('--vocab_path',
                           help='Path to model vocabulary')
     from_cmd.add_argument('--module_path',
                           help='Path to folder containing model module')
+    from_cmd.add_argument('--device',
+                          help='(optional) Torch device name on which model will be run.'
+                               'Defaults to cpu.')
+
+    # GoogleLM args
+    from_cmd.add_argument('--pbtxt_path',
+                          help='Path to the .pbtxt file containing the GraphDef model setup.')
+    from_cmd.add_argument('--ckpt_dir',
+                          help='Path to folder containing parameter checkpoint files.')
+    from_cmd.add_argument('--corpus_vocab_path',
+                          help='Path to the corpus vocabulary. This allows for only a subset of the'
+                               'model softmax to be loaded in.')
+    from_cmd.add_argument('--full_vocab_path',
+                          help='Path to the full model vocabulary of 800k tokens.')
+
+    # Corpus args
     from_cmd.add_argument('--corpus_path',
                           help='Path to labeled corpus')
-    # TODO: Provide explanation of activation names
-    from_cmd.add_argument('--activation_names',
-                          help='Activations to be extracted', nargs='*')
-    from_cmd.add_argument('--output_dir',
-                          help='Path to folder to which extracted embeddings will be written.')
     from_cmd.add_argument('--corpus_header', nargs='*',
                           help='(optional) List of corpus attribute names.'
                                'If not provided first line is used as header.')
@@ -42,9 +56,13 @@ def init_argparser() -> ArgumentParser:
     from_cmd.add_argument('--from_dict', type=bool,
                           help='(optional) Set to true to load in pickled corpus dictionary, '
                                'instead of a raw file.')
-    from_cmd.add_argument('--device',
-                          help='(optional) Torch device name on which model will be run.'
-                               'Defaults to cpu.')
+
+    # Extraction args
+    # TODO: Provide explanation of activation names
+    from_cmd.add_argument('--activation_names',
+                          help='Activations to be extracted', nargs='*')
+    from_cmd.add_argument('--output_dir',
+                          help='Path to folder to which extracted embeddings will be written.')
     from_cmd.add_argument('--init_lstm_states_path',
                           help='(optional) Location of initial lstm states of the model. '
                                'If no path is provided zero-initialized states will be used at the'
@@ -72,7 +90,7 @@ if __name__ == '__main__':
     required_args = {'model_type', 'corpus_path', 'activation_names', 'output_dir'}
     arg_groups = {
         'model': {'model_type', 'model_path', 'vocab_path', 'module_path', 'pbtxt_path', 'ckpt_dir',
-                  'full_vocab_path', 'device'},
+                  'corpus_vocab_path', 'full_vocab_path', 'device'},
         'corpus': {'corpus_path', 'corpus_header', 'to_lower', 'from_dict'},
         'init_extract': {'activation_names', 'output_dir', 'init_lstm_states_path'},
         'extract': {'cutoff', 'print_every', 'dynamic_dumping', 'create_label_file',
