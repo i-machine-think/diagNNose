0783a2dab2734ba2e6d1d04d1b8bc0bde09c2e1e
jumelet <jumeletjaap@gmail.com>
2019-06-24 02:31:02 +0200

    :scissors: Remove nltk requirement

--- setup.py
+++ setup.py
@@ -63,8 +63,7 @@ setup(
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=['numpy',
                       'torch',
-                      'scipy',
-                      'nltk'],
+                      'scipy'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
