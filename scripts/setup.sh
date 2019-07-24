396a2207252a4b889afeb356125b73c17ae3773e
jumelet <jumeletjaap@gmail.com>
2019-07-24 12:01:30 +0200

    :rotating_light: Update model url for integration test

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,10 @@
-# Download model
+#!/usr/bin/env bash
+# Download vocab
 wget https://dl.fbaipublicfiles.com/colorless-green-rnns/training-data/English/vocab.txt -P model
 
 # Download state_dict of gulordava lstm, as of now stored on google drive
-fileid="1SdIHXZzzubWbI7DXkMmHe0hMATtiPngX"
+# The model that is stored on the fb server is not used here, as it isn't the saved state dict but the model class itself.
+fileid="1w47WsZcZzPyBKDn83cMNd0Hb336e-_Sy"
 filename="model/state_dict.pt"
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
@@ -12,4 +14,3 @@ mkdir extracted
 
 # Download data
 # TODO I just put a mini corpus here now, but we should put it somewhere else and download it too
-
