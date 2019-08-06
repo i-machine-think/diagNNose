8f28bba8766000de02ecdc2eeed895133de736e7
jumelet <jumeletjaap@gmail.com>
2019-08-07 00:57:14 +0200

    :art: Update CD attention scripts

--- scripts/visual_cd.py
+++ scripts/visual_cd.py
@@ -3,12 +3,20 @@ from diagnnose.config.setup import ConfigSetup
 from diagnnose.corpus.import_corpus import import_corpus
 from diagnnose.decompositions.attention import CDAttention
 from diagnnose.models.import_model import import_model
-from diagnnose.typedefs.corpus import Corpus
 from diagnnose.models.lm import LanguageModel
+from diagnnose.typedefs.corpus import Corpus
 from diagnnose.vocab import get_vocab_from_config
 
 if __name__ == "__main__":
-    arg_groups = {"model", "activations", "corpus", "init_states", "vocab", "decompose"}
+    arg_groups = {
+        "model",
+        "activations",
+        "corpus",
+        "init_states",
+        "vocab",
+        "decompose",
+        "plot_attention",
+    }
     arg_parser, required_args = create_arg_parser(arg_groups)
 
     config_dict = ConfigSetup(arg_parser, required_args, arg_groups).config_dict
@@ -20,9 +28,13 @@ if __name__ == "__main__":
         vocab_path=get_vocab_from_config(config_dict), **config_dict["corpus"]
     )
 
-    attention = CDAttention(model, cd_config=config_dict["decompose"])
+    attention = CDAttention(
+        model,
+        corpus,
+        cd_config=config_dict["decompose"],
+        plot_config=config_dict["plot_attention"],
+    )
 
-    sen_id = 21
-
-    attention.plot_by_sen_id(corpus, sen_id, **config_dict["activations"])
-    attention.plot_by_sen_id(corpus, sen_id + 1, **config_dict["activations"])
+    attention.plot_by_sen_id(
+        slice(1200, 2400, 2), avg_decs=True, **config_dict["activations"]
+    )
