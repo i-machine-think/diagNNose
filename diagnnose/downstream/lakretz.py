74fd62077c866b7fc969be4849d41393d93197ac
jumelet <jumeletjaap@gmail.com>
2019-08-12 18:30:35 +0200

    :sparkles: Batchify Lakretz task

--- diagnnose/downstream/lakretz.py
+++ diagnnose/downstream/lakretz.py
@@ -4,6 +4,7 @@ from typing import Any, Dict, List, Optional
 
 import numpy as np
 import torch
+from torch import Tensor
 
 from diagnnose.activations.activation_reader import ActivationReader
 from diagnnose.corpus.create_iterator import create_iterator
@@ -32,9 +33,9 @@ lakretz_descriptions: Dict[str, Any] = {
 def lakretz_downstream(
     model: LanguageModel,
     vocab_path: str,
-    lakretz_path: str,
-    lakretz_activations: Optional[Dict[str, str]] = None,
-    lakretz_tasks: Optional[List[str]] = None,
+    path: str,
+    task_activations: Optional[Dict[str, str]] = None,
+    tasks: Optional[List[str]] = None,
     device: str = "cpu",
     print_results: bool = True,
     ignore_unk: bool = True,
@@ -50,14 +51,14 @@ def lakretz_downstream(
         Language model for which the accuracy is calculated.
     vocab_path : str
         Path to vocabulary file of the Language Model.
-    lakretz_path : str
+    path : str
         Path to directory containing the datasets that can be found
         in the github repo.
-    lakretz_activations : str, optional
+    task_activations : str, optional
         Dictionary mapping task names to directories to which the
         Lakretz task embeddings have been extracted. If a task is not
         provided the activations will be created during the task.
-    lakretz_tasks : List[str], optional
+    tasks : List[str], optional
         The downstream tasks that will be tested. If not provided this
         will default to the full set of conditions.
     device : str, optional
@@ -74,95 +75,99 @@ def lakretz_downstream(
         Dictionary mapping a downstream task to a task condition to the
         model accuracy.
     """
-    lakretz_activations = lakretz_activations or {}
+    task_activations = task_activations or {}
 
-    if lakretz_tasks is None:
-        lakretz_tasks = list(lakretz_descriptions.keys())
+    if tasks is None:
+        tasks = list(lakretz_descriptions.keys())
 
     accs_dict: Dict[str, Dict[str, float]] = {}
 
-    for task in lakretz_tasks:
+    for task in tasks:
         assert task in lakretz_descriptions, f"Provided task {task} is not recognised!"
 
-        activation_dir = lakretz_activations.get(task, None)
+        activation_dir = task_activations.get(task, None)
         activation_reader = (
             ActivationReader(activation_dir) if activation_dir is not None else None
         )
 
         if print_results:
-            print(task)
+            print(f"\n{task}")
         condition_specs = lakretz_descriptions[task]
         items_per_class = condition_specs["items_per_class"]
 
         corpus = import_corpus(
-            os.path.join(lakretz_path, f"{task}.txt"),
+            os.path.join(path, f"{task}.txt"),
             corpus_header=["sen", "type", "correct", "idx"],
             vocab_path=vocab_path,
         )
 
-        iterator = create_iterator(corpus, batch_size=2, device=device)
+        sen_len = len(corpus.examples[0].sen)
+        iterator = create_iterator(
+            corpus, batch_size=(items_per_class * 2), device=device
+        )
 
         skipped = 0
         accs = np.zeros(condition_specs["classes"])
-        tots = np.zeros(condition_specs["classes"])
 
         for i, batch in enumerate(iterator):
-            if print_results and i % items_per_class == 1 and i > 1:
-                prev_condition = ((i - 1) // items_per_class) - 1
-
-                prev_acc = accs[prev_condition] / tots[prev_condition]
-                skipped = items_per_class - tots[prev_condition]
-                print(
-                    f"{condition_specs['conditions'][prev_condition]}:\t{prev_acc:.3f}"
-                )
-                if skipped:
-                    print(f"{skipped:.0f}/{items_per_class} items were skipped.")
-
             if activation_dir is None:
-                if model.use_char_embs:
-                    sen = corpus.examples[i * 2].sen[:-1]
-                else:
-                    sen = batch.sen[0][0][:-1]
-
-                hidden = model.init_hidden(1)
-                for w in sen:
-                    w = [w] if model.use_char_embs else w.view(1)
+                hidden = model.init_hidden(items_per_class)
+                for j in range(sen_len - 1):
+                    if model.use_char_embs:
+                        w = [
+                            corpus.examples[k].sen[j]
+                            for k in range(
+                                i * items_per_class * 2,
+                                (i + 1) * items_per_class * 2,
+                                2,
+                            )
+                        ]
+                    else:
+                        w = batch.sen[0][::2, j]
                     with torch.no_grad():
                         _, hidden = model(w, hidden, compute_out=False)
 
                 final_hidden = model.final_hidden(hidden)
             else:
-                assert activation_reader is not None  # mypy being annoying
-                final_hidden = activation_reader[
-                    i * 2, {"a_name": (model.num_layers - 1, "hx")}
-                ][0][-2]
+                sen_slice = slice(
+                    i * items_per_class * 2, (i + 1) * items_per_class * 2, 2
+                )
+                final_hidden = torch.stack(
+                    activation_reader[sen_slice, {"a_name": (model.top_layer, "hx")}],
+                    dim=0,
+                )[:, -2, :]
 
-            w1 = batch.sen[0][0][-1]
-            w2 = batch.sen[0][1][-1]
+            w1: Tensor = batch.sen[0][::2, -1]
+            w2: Tensor = batch.sen[0][1::2, -1]
 
             if ignore_unk:
-                if w1 == corpus.vocab.stoi.unk_idx:
-                    token = corpus.examples[i * 2].sen[-1]
-                    warnings.warn(f"'{token}' is not part of model vocab!")
-                    continue
-                if w2 == corpus.vocab.stoi.unk_idx:
-                    token = corpus.examples[i * 2 + 1].sen[-1]
-                    warnings.warn(f"'{token}' is not part of model vocab!")
-                    continue
+                unk_idx = corpus.vocab.stoi.unk_idx
+                for widx, mask_vals in enumerate(zip((w1 == unk_idx), (w2 == unk_idx))):
+                    if mask_vals[0] == 1:
+                        token = corpus.examples[widx * 2].sen[-1]
+                        warnings.warn(f"'{token}' is not part of model vocab!")
+                    if mask_vals[1] == 1:
+                        token = corpus.examples[widx * 2 + 1].sen[-1]
+                        warnings.warn(f"'{token}' is not part of model vocab!")
+                mask = (w1 == unk_idx) | (w2 == unk_idx)
+                skipped = int(torch.sum(mask))
+                w1 = w1[~mask]
+                w2 = w2[~mask]
+                final_hidden = final_hidden[~mask]
 
-            classes = [[w1, w2]]
+            classes = torch.stack((w1, w2), dim=1)
 
-            probs = model.decoder_w[classes] @ final_hidden
+            probs = torch.bmm(model.decoder_w[classes], final_hidden.unsqueeze(2))
+            probs = probs[:, :, 0]
             probs += model.decoder_b[classes]
 
-            if probs[0] >= probs[1]:
-                accs[i // items_per_class] += 1
-            tots[i // items_per_class] += 1
+            accs[i] += torch.mean((probs[:, 0] >= probs[:, 1]).to(torch.float))
 
-        if print_results:
-            print(f"{condition_specs['conditions'][-1]}:\t{accs[-1]/tots[-1]:.3f}\n")
-            if skipped:
-                print(f"{skipped:.0f}/{items_per_class} items were skipped.")
-        accs_dict[task] = dict(zip(condition_specs["conditions"], accs / tots))
+            if print_results:
+                print(f"{condition_specs['conditions'][i]}:\t{accs[i]:.3f}")
+                if skipped > 0:
+                    print(f"{skipped:.0f}/{items_per_class} items were skipped.\n")
+
+        accs_dict[task] = dict(zip(condition_specs["conditions"], accs))
 
     return accs_dict
