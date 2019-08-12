9cac0699e7c9513dbc220ee066b930ffdbca85f1
jumelet <jumeletjaap@gmail.com>
2019-08-12 18:34:59 +0200

    :sparkles: Allow sorted corpus iterator

--- diagnnose/corpus/create_iterator.py
+++ diagnnose/corpus/create_iterator.py
@@ -1,13 +1,29 @@
+from typing import Callable, Optional
+
 from torchtext.data import BucketIterator
 
 from diagnnose.typedefs.corpus import Corpus
 
 
 def create_iterator(
-    corpus: Corpus, batch_size: int = 1, device: str = "cpu"
+    corpus: Corpus,
+    batch_size: int = 1,
+    device: str = "cpu",
+    sort: Optional[bool] = None,
 ) -> BucketIterator:
+    if sort is not None:
+        sort_key: Optional[Callable] = lambda e: len(e.sen)
+        print("Sorting corpus on sentence length...")
+    else:
+        sort_key = None
+
     iterator = BucketIterator(
-        dataset=corpus, batch_size=batch_size, device=device, shuffle=False
+        dataset=corpus,
+        batch_size=batch_size,
+        device=device,
+        shuffle=False,
+        sort=sort,
+        sort_key=sort_key,
     )
 
     return iterator
