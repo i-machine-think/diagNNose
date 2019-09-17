bab466932e2d12d726be34bc4f640fe90a42c031
jumelet <jumeletjaap@gmail.com>
2019-09-17 17:36:42 +0200

    :sparkles: Improve functionality CD attention

--- diagnnose/decompositions/attention.py
+++ diagnnose/decompositions/attention.py
@@ -7,18 +7,17 @@ import numpy as np
 import torch
 from torch import Tensor
 
+import diagnnose.typedefs.config as config
 from diagnnose.activations.activation_index import (
     activation_index_len,
     activation_index_to_iterable,
 )
 from diagnnose.decompositions import DecomposerFactory
 from diagnnose.decompositions.base_decomposer import BaseDecomposer
-from diagnnose.extractors.base_extractor import Extractor
 from diagnnose.models.lm import LanguageModel
-from diagnnose.typedefs.activations import ActivationIndex, ActivationNames
+from diagnnose.typedefs.activations import ActivationIndex
 from diagnnose.typedefs.corpus import Corpus
 from diagnnose.utils.midpoint import MidPointNorm
-from diagnnose.utils.misc import suppress_print
 
 plt.rcParams["figure.figsize"] = 15, 10
 
@@ -39,10 +38,8 @@ class CDAttention:
         self.include_init = include_init
 
         if cd_config is None:
-            self.decomposer = "ContextualDecomposer"
             self.cd_config: Dict[str, Any] = {}
         else:
-            self.decomposer = cd_config.pop("decomposer", "ContextualDecomposer")
             self.cd_config = cd_config
 
         if plot_config is None:
@@ -55,6 +52,7 @@ class CDAttention:
         sen_ids: ActivationIndex,
         activations_dir: Optional[str] = None,
         avg_decs: bool = False,
+        extra_classes: Optional[List[int]] = None,
         arr_pickle: Optional[str] = None,
         save_arr_as: Optional[str] = None,
         save_plot_as: Optional[str] = None,
@@ -63,19 +61,27 @@ class CDAttention:
             arr: Tensor = torch.load(arr_pickle)
         else:
             arr = self.calc_by_sen_id(
-                sen_ids, activations_dir=activations_dir, save_arr_as=save_arr_as
+                sen_ids,
+                activations_dir=activations_dir,
+                extra_classes=extra_classes,
+                save_arr_as=save_arr_as,
             )
 
         if avg_decs:
             avg_arr = torch.mean(arr, dim=0)
-            sen = self.plot_config.get("sen", None)
-            self.plot_attention(avg_arr, sen=sen, save_plot_as=save_plot_as)
+            self.plot_attention(avg_arr, save_plot_as=save_plot_as)
         else:
             sen_ids = activation_index_to_iterable(sen_ids)
             batch_size = arr.size(0)
+            y_idx = -len(extra_classes or []) - 1
             for i in range(batch_size):
-                sen_id = sen_ids[i]  # mypy error fixable with PEP 544
-                self.plot_attention(arr[i], sen=self.corpus[sen_id].sen)
+                self.plot_config.update(
+                    {
+                        "xtext": self.corpus[sen_ids[i]].sen[1:],
+                        "ytext": self.corpus[sen_ids[i]].sen[:y_idx],
+                    }
+                )
+                self.plot_attention(arr[i])
 
         return arr
 
@@ -83,18 +89,29 @@ class CDAttention:
         self,
         sen_ids: ActivationIndex,
         activations_dir: Optional[str] = None,
+        extra_classes: Optional[List[int]] = None,
         save_arr_as: Optional[str] = None,
     ) -> Tensor:
         if isinstance(sen_ids, int):
             sen_ids = [sen_ids]
         classes = self._create_output_classes(sen_ids)
-        factory = self._create_factory(sen_ids, activations_dir)
+
         if activations_dir is None:
             # We index by extraction position, so if N non-consecutive corpus items are being
             # decomposed we should pass a slice of N items along from here.
             sen_ids = slice(0, activation_index_len(sen_ids), 1)
 
-        decomposer = factory.create(sen_ids, classes=classes)
+        factory = DecomposerFactory(
+            self.model,
+            activations_dir or TMP_DIR,
+            create_new_activations=(activations_dir is None),
+            corpus=self.corpus,
+            sen_ids=sen_ids,
+        )
+
+        decomposer = factory.create(
+            sen_ids, classes=classes, extra_classes=extra_classes
+        )
 
         arr = self.calc_attention(decomposer)
 
@@ -106,56 +123,44 @@ class CDAttention:
 
         return arr
 
-    def calc_attention(
-        self, decomposer: BaseDecomposer
-    ) -> Tensor:
+    def calc_attention(self, decomposer: BaseDecomposer) -> Tensor:
         start_id = 0 if self.include_init else 1
         sen_len = int(decomposer.final_index[0])
-        normalize = self.cd_config.pop('normalize', True)
+        normalize = self.cd_config.pop("normalize", True)
 
         # Number of input features to be decomposed (init + w0 -- wn-1)
-        ndecomp = sen_len - 1 + int(self.include_init) + 3
+        ndecomp = sen_len - 1 + int(self.include_init)
         # Number of output classes (w1 -- wn)
-        noutput = sen_len - 1
+        noutput = sen_len - 1 + len(decomposer.extra_classes)
 
-        rel_scores = torch.zeros((decomposer.batch_size, ndecomp, noutput))
-        irrel_scores = torch.zeros((decomposer.batch_size, ndecomp, noutput))
+        rel_scores = torch.zeros(
+            (decomposer.batch_size, ndecomp, noutput), dtype=config.DTYPE
+        )
+        irrel_scores = torch.zeros(
+            (decomposer.batch_size, ndecomp, noutput), dtype=config.DTYPE
+        )
 
         for i in range(start_id, sen_len):
             # Note that this can slightly be improved, as we calculate the decoder score for each
             # output class at each time step. But for now this suffices.
-            # Shape: (batch_size, ninput, noutput
+            # Shape: (batch_size, ninput, noutput)
             decomposition = decomposer.decompose(i - 1, i, **self.cd_config)
             if not self.include_init:
                 i -= 1
-            rel_scores[:, i] = torch.diagonal(
-                decomposition["relevant"][:, :, 1:], dim1=1, dim2=2
-            )
+            rel_scores[:, i] = torch.diagonal(decomposition["relevant"], dim1=1, dim2=2)
             irrel_scores[:, i] = torch.diagonal(
-                decomposition["irrelevant"][:, :, 1:], dim1=1, dim2=2
+                decomposition["irrelevant"], dim1=1, dim2=2
             )
 
-        bias = decomposer.decoder_b[:, 1:].unsqueeze(1)
+        bias = decomposer.decoder_b.unsqueeze(1)
         if normalize:
             norm_scores = rel_scores / (rel_scores + irrel_scores + bias)
             return norm_scores
 
-        rel_scores[:, -3] = torch.sum(rel_scores, dim=1)
-        rel_scores[:, -2] = rel_scores[:, 0] + irrel_scores[:, 0]
-        rel_scores[:, -1] = rel_scores[:, -3] / rel_scores[:, -2]
-
-        for i in range(rel_scores.size(0)):
-            rel_scores[i, :-3] /= rel_scores[i, -3]
-
         self.cd_config["normalize"] = False
         return rel_scores
 
-    def plot_attention(
-        self,
-        arr: Tensor,
-        sen: Optional[List[str]] = None,
-        save_plot_as: Optional[str] = None,
-    ) -> None:
+    def plot_attention(self, arr: Tensor, save_plot_as: Optional[str] = None) -> None:
         arr = arr.numpy()
         arr_mask = np.ma.masked_array(arr, mask=(arr != 0.0))
         arr = np.ma.masked_array(arr, mask=(arr == 0))
@@ -184,21 +189,24 @@ class CDAttention:
         )
         ax.imshow(arr_mask, cmap="gist_yarg")
 
-        if sen is not None:
-            xtext = sen[1:]
-            ytext = sen[:-1]
-            if self.include_init:
-                ytext = ["INIT"] + ytext
+        if "xtext" in self.plot_config:
+            xtext = self.plot_config["xtext"]
 
             ax.set_xticks(range(len(xtext)))
             ax.set_xticklabels(xtext, rotation=35, ha="left", rotation_mode="anchor")
-            ax.set_yticks(range(len(ytext)))
-            ax.set_yticklabels(ytext)
-
-            ax.tick_params(axis="y", which="both", labelsize=30)
-            ax.tick_params(axis="x", which="both", labelsize=26)
+            ax.tick_params(axis="x", which="both", labelsize=32)
         else:
             ax.set_xticks([])
+
+        if "ytext" in self.plot_config:
+            ytext = self.plot_config["ytext"]
+            if self.include_init:
+                ytext = ["INIT"] + ytext
+
+            ax.set_yticks(range(len(ytext)))
+            ax.set_yticklabels(ytext)
+            ax.tick_params(axis="y", which="both", labelsize=32)
+        else:
             ax.set_yticks([])
 
         if self.plot_config.get("plot_values", True):
@@ -206,21 +214,51 @@ class CDAttention:
             for (j, i), label in np.ndenumerate(arr):
                 if label == 0.0:
                     continue
-                beta = np.round(label, 2)
-                if (cmin / 1.5) < beta < (cmax / 1.3):
+                if (cmin / 1.4) < label < (cmax / 1.4):
                     ax.text(
-                        i, j, beta, ha="center", va="center", fontsize=fs, color="black"
+                        i,
+                        j,
+                        f"{label:.2f}",
+                        ha="center",
+                        va="center",
+                        fontsize=fs,
+                        color="black",
                     )
                 else:
                     ax.text(
-                        i, j, beta, ha="center", va="center", fontsize=fs, color="white"
+                        i,
+                        j,
+                        f"{label:.2f}",
+                        ha="center",
+                        va="center",
+                        fontsize=fs,
+                        color="white",
                     )
 
-        ax.set_ylabel("Decomposed token", fontsize=24)
-        ax.set_xlabel("Predicted class", fontsize=24)
+        ax.set_ylabel("Decomposed token", fontsize=28)
+        ax.set_xlabel("Predicted class", fontsize=28)
 
         ax.xaxis.tick_top()
         ax.xaxis.set_label_position("top")
+
+        if "xtext_colors" in self.plot_config:
+            for idx, color in self.plot_config["xtext_colors"]:
+                ax.get_xticklabels()[idx].set_color(color)
+        if "ytext_colors" in self.plot_config:
+            for idx, color in self.plot_config["ytext_colors"]:
+                ax.get_yticklabels()[idx].set_color(color)
+
+        for patch_x, patch_y in self.plot_config.get("patches"):
+            ax.add_patch(
+                patches.Rectangle(
+                    (patch_x - 0.47, patch_y - 0.48),
+                    0.95,
+                    0.94,
+                    linewidth=4,
+                    edgecolor="black",
+                    facecolor="none",
+                )
+            )
 
         if self.plot_config.get("title", None) is not None:
             title = ax.set_title(self.plot_config["title"], fontsize=30)
