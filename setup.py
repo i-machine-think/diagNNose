377692124651b87957ccf3fee37a551345b496e1
jumelet <jumeletjaap@gmail.com>
2020-01-23 16:41:06 +0100

    :bug: Add updated setup.py

--- setup.py
+++ setup.py
@@ -9,49 +9,29 @@ here = path.abspath(path.dirname(__file__))
 with codecs.open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
+on_rtd = os.environ.get('READTHEDOCS') == 'True'
+if on_rtd:
+    install_requires = []
+else:
+    with open('requirements.txt') as f:
+        install_requires = [l.strip() for l in f]
+
 setup(
     name="diagnnose",
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
     version="0.1-alpha",
     description="A library that facilitates a broad set of tools for analysing "
                 "hidden activations of neural models.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    # The project's main homepage.
-    # url='https://github.com/dieuwkehupkes/diagnosing_lms',
-    # Choose your license
     license="Apache License 2.0",
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        # 'Development Status :: 3 - Alpha',
-        # Indicate who your project is intended for
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
-        # Pick your license as you wish (should match "license" above)
-        # 'License :: Apache License 2.0',
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
         "Programming Language :: Python :: 3.7",
     ],
-    # What does your project relate to?
     keywords="introspection, analysis, deep learning, nlp",
     url="https://github.com/i-machine-think/diagnnose",
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
     packages=find_packages(exclude=["contrib", "docs", "test", "scripts"]),
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    #   py_modules=["my_module"],
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
         "numpy>=1.16.2",
         "torch>=1.1.0",
@@ -63,9 +43,5 @@ setup(
         "unidecode",
     ],
     python_requires=">=3.7.0",
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
     extras_require={"dev": ["check-manifest"], "test": ["coverage"]},
 )
