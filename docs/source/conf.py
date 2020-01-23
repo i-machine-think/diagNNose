87bf5cdc0c125552d4bdfd4da72c786caead5c72
jumelet <jumeletjaap@gmail.com>
2020-01-23 14:20:11 +0100

    :books: Update rtd yml

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,18 +14,8 @@
 #
 import os
 import sys
-#sys.path.insert(0, os.path.abspath('.'))
 
 sys.path.append(os.path.join(os.path.dirname(__name__), ".."))
-
-#sys.path.insert(0, "../..")
-
-#modules = [
-#    "decompositions"
-#]
-
-#for m in modules:
-#    sys.path.insert(0, f"../../diagnnose/{m}")
 
 # -- Project information -----------------------------------------------------
 
