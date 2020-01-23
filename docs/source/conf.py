d7b40b1e187162b0016c15174a7a4013769bcd48
jumelet <jumeletjaap@gmail.com>
2020-01-23 16:38:44 +0100

    :books: Update rtd config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -177,4 +177,4 @@ epub_exclude_files = ['search.html']
 
 # -- Extension configuration -------------------------------------------------
 
-autodoc_mock_imports = ['torch', 'skorch', 'torchtext']
+autodoc_mock_imports = ['torch', 'skorch', 'torchtext', 'sklearn']
