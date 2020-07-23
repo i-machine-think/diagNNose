5c34497dd5fdca30754856409ac3c27cd8a99d73
jumelet <jumeletjaap@gmail.com>
2020-07-23 14:31:21 +0200

    :recycle: Make corpus iterator a general Iterator

--- diagnnose/corpus/create_iterator.py
+++ diagnnose/corpus/create_iterator.py
@@ -1,28 +1,26 @@
 from typing import Callable, Optional
 
-from torchtext.data import BucketIterator
+from torchtext.data import Iterator
 
 from diagnnose.corpus import Corpus
 
 
 def create_iterator(
-    corpus: Corpus,
-    batch_size: int = 1,
-    device: str = "cpu",
-    sort: Optional[bool] = None,
-) -> BucketIterator:
+    corpus: Corpus, batch_size: int = 1, device: str = "cpu", sort: bool = False
+) -> Iterator:
     if sort is not None:
-        sort_key: Optional[Callable] = lambda e: len(e.sen)
+        sort_key: Optional[Callable] = lambda ex: len(getattr(ex, corpus.sen_column))
     else:
         sort_key = None
 
-    iterator = BucketIterator(
+    iterator = Iterator(
         dataset=corpus,
         batch_size=batch_size,
         device=device,
         shuffle=False,
         sort=sort,
         sort_key=sort_key,
+        train=False,
     )
 
     return iterator
