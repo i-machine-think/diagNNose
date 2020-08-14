a7395e315f9f89a614bf93781b665a813010168a
jumelet <jumeletjaap@gmail.com>
2020-08-14 19:21:20 +0200

    :recycle: Update downstream scripts

--- scripts/downstream.py
+++ scripts/downstream.py
@@ -8,8 +8,7 @@ from diagnnose.models.import_model import import_model
 from diagnnose.tokenizer import Tokenizer, create_tokenizer
 
 if __name__ == "__main__":
-    arg_groups = {"model", "vocab", "downstream", "init_states"}
-    arg_parser, required_args = create_arg_parser(arg_groups)
+    arg_parser, required_args = create_arg_parser({"model", "downstream"})
     config_dict = create_config_dict(arg_parser, required_args)
 
     # Only pass along the selected set of tasks in the config
