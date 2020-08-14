ac100b176d8e1384f1e5ae82d75ab14d503d40a4
jumelet <jumeletjaap@gmail.com>
2020-08-14 19:21:52 +0200

    :recycle: Update tokenizer __init__.py

---
+++ diagnnose/tokenizer/__init__.py
@@ -0,0 +1,2 @@
+from .create import create_tokenizer, create_char_vocab
+from .tokenizer import Tokenizer
--- diagnnose/vocab/__init__.py
+++
@@ -1,4 +0,0 @@
-from diagnnose.vocab.c2i import C2I
-from diagnnose.vocab.w2i import W2I
-from diagnnose.vocab.create import create_vocab, create_char_vocab
-from diagnnose.vocab.misc import get_vocab_path_from_config
