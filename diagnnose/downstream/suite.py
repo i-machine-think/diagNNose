1f3b2141282cf7d154675963d9776c0e5a93715a
jumelet <jumeletjaap@gmail.com>
2020-09-14 17:34:30 +0200

    :recycle: Move general downstream config to top-level of Suite

--- diagnnose/downstream/suite.py
+++ diagnnose/downstream/suite.py
@@ -13,7 +13,7 @@ from diagnnose.downstream.tasks import (
 )
 from diagnnose.models import LanguageModel
 
-task_constructors: Dict[str, Callable] = {
+task_constructors: Dict[str, Type[DownstreamTask]] = {
     "lakretz": LakretzDownstream,
     "linzen": LinzenDownstream,
     "marvin": MarvinDownstream,
@@ -33,43 +33,41 @@ class DownstreamSuite:
 
     Parameters
     ----------
-    downstream_config : Dict[str, Any]
+    config : Dict[str, Any]
         Dictionary mapping a task name (`lakretz`, `linzen`, `marvin`,
         or `winobias`) to its configuration (`path`, `tasks`, and
         `task_activations`). `path` points to the corpus folder of the
         task, `tasks` is an optional list of subtasks, and
         `task_activations` an optional path to the folder containing
         the model activations.
-    tokenizer : Tokenizer
+    tokenizer : PreTrainedTokenizer
         Tokenizer that converts tokens to their index within the LM.
-    decompose_config : Dict[str, Any], optional
-        Optional setup to perform contextual decomposition on the
-        activations prior to executing the downstream tasks.
     """
 
     def __init__(
         self,
         model: LanguageModel,
-        downstream_config: Dict[str, Any],
-        tokenizer: Tokenizer,
-        decompose_config: Optional[Dict[str, Any]] = None,
+        tokenizer: PreTrainedTokenizer,
+        config: Dict[str, Any],
+        ignore_unk: bool = False,
+        tasks: Optional[List[str]] = None,
     ) -> None:
-        self.downstream_config = downstream_config
-        self.decompose_config = decompose_config
-
+        self.ignore_unk = ignore_unk
         self.tasks: Dict[str, DownstreamTask] = {}
 
         print("Initializing downstream tasks...")
 
-        for task_name, config in self.downstream_config.items():
+        for task_name in tasks or config.keys():
+            task_config = config[task_name]
+
             # If a single subtask is passed as cmd arg it is not converted to a list yet
-            subtasks = config.pop("subtasks", None)
+            subtasks = task_config.pop("subtasks", None)
             if isinstance(subtasks, str):
                 subtasks = [subtasks]
 
             constructor = task_constructors[task_name]
             self.tasks[task_name] = constructor(
-                model, tokenizer, config.pop("path"), subtasks=subtasks
+                model, tokenizer, task_config.pop("path"), subtasks=subtasks
             )
 
         print("Downstream task initialization finished")
@@ -80,6 +78,6 @@ class DownstreamSuite:
         for task_name, task in self.tasks.items():
             print(f"\n--=={task_name.upper()}==--")
 
-            results[task_name] = task.run(**kwargs)
+            results[task_name] = task.run(ignore_unk=self.ignore_unk, **kwargs)
 
         return results
