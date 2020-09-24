c7db69375c43266037d0ec8d843d6c7e8a85ec5a
jumelet <jumeletjaap@gmail.com>
2020-09-24 10:54:23 +0200

    :recycle: Update explain script

--- scripts/explain.py
+++ scripts/explain.py
@@ -1,17 +1,15 @@
-from diagnnose.config.arg_parser import create_arg_parser
-from diagnnose.config.setup import create_config_dict
-from diagnnose.attribute import MaskedLMExplainer
+from diagnnose.attribute.explainer import Explainer
+from diagnnose.config.config_dict import create_config_dict
 from diagnnose.models import LanguageModel
 from diagnnose.models.import_model import import_model
 from diagnnose.tokenizer import create_tokenizer
 
 if __name__ == "__main__":
-    arg_parser, required_args = create_arg_parser({"model"})
-    config_dict = create_config_dict(arg_parser, required_args)
+    config_dict = create_config_dict()
 
     model: LanguageModel = import_model(config_dict)
-    tokenizer = create_tokenizer(config_dict["tokenizer"])
+    tokenizer = create_tokenizer(**config_dict["tokenizer"])
 
-    explainer = MaskedLMExplainer(model, tokenizer, decomposer="decomposer")
+    explainer = Explainer(model, tokenizer, decomposer="shapley_decomposer")
 
-    explainer.explain(["The boy <mask>.", "The boys <mask>."], ["walk", "walks"])
+    explainer.explain(["The athletes above Barbara <mask>."], ["approve", "approves"])
