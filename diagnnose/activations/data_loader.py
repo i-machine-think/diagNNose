ba4ee6f78e7cb4a692d26ecb98752abe6f8b15e9
jumelet <jumeletjaap@gmail.com>
2020-11-12 15:28:54 +0100

    :recycle: Refactor DataLoader

--- diagnnose/activations/data_loader.py
+++ diagnnose/activations/data_loader.py
@@ -1,20 +1,34 @@
+import random
+from collections import namedtuple
 from typing import Optional, Tuple
 
-import numpy as np
 import torch
-from torch import Tensor
 from torchtext.vocab import Vocab
 
 from diagnnose.corpus import Corpus
 from diagnnose.corpus.create_labels import create_labels_from_corpus
-from diagnnose.typedefs.activations import ActivationName, SelectionFunc
+from diagnnose.typedefs.activations import (
+    ActivationIndex,
+    ActivationName,
+    SelectionFunc,
+)
 from diagnnose.typedefs.probe import ControlTask, DataDict
 
 from .activation_reader import ActivationReader
 
 
+DataSplit = namedtuple("DataSplit", ["activation_reader", "labels", "control_labels"])
+
+
 class DataLoader:
-    """Reads in pickled activations that have been extracted.
+    """Reads in pickled activations that have been extracted, and
+    creates a train/test split of activations and labels.
+
+    Train/test split can be created in multiple ways:
+    1. Using the test activations from a different corpus
+    2. Using the activations from the same corpus for both, but the
+    train/test split is defined on 2 separate selection_funcs.
+    3. Based on a random 90/10 split.
 
     Parameters
     ----------
@@ -42,76 +56,155 @@ class DataLoader:
     control_task: ControlTask, optional
         Control task function of Hewitt et al. (2019), mapping a corpus
         item to a random label.
+    train_test_ratio : float, optional
+        Ratio of the train/test split. If separate test
+        activations are provided this split won't be used.
+        Defaults to None, but must be provided if no test_selection_func
+        is passed.
     """
 
-    # TODO: Move init logic to own method
     def __init__(
         self,
         activations_dir: str,
         corpus: Corpus,
+        labels_column: str,
         test_activations_dir: Optional[str] = None,
         test_corpus: Optional[Corpus] = None,
-        train_selection_func: SelectionFunc = lambda sen_id, pos, example: True,
+        test_labels_column: Optional[str] = None,
+        train_selection_func: Optional[SelectionFunc] = None,
         test_selection_func: Optional[SelectionFunc] = None,
         control_task: Optional[ControlTask] = None,
+        train_test_ratio: Optional[float] = None,
     ) -> None:
-        assert corpus is not None, "`corpus`should be provided!"
-
-        self.train_labels = create_labels_from_corpus(
-            corpus, selection_func=train_selection_func
+        assert (
+            (test_corpus is not None and test_activations_dir is not None)
+            or test_selection_func is not None
+            or train_test_ratio is not None
         )
-        self.test_labels = None
-        self.train_labels_control = None
-        self.test_labels_control = None
+
+        train_activation_reader = ActivationReader(
+            activations_dir, cat_activations=True
+        )
+
+        self.train_ids, self.test_ids = self._train_test_ids(
+            corpus,
+            train_activation_reader.selection_func,
+            train_selection_func,
+            test_selection_func,
+            train_test_ratio,
+        )
+
+        self.train_split = self._create_data_split(
+            train_activation_reader,
+            corpus,
+            labels_column,
+            self.train_ids,
+            control_task,
+        )
+
+        self.test_split = self._create_test_split(
+            corpus,
+            labels_column,
+            train_activation_reader,
+            test_activations_dir,
+            test_corpus,
+            test_labels_column,
+            test_selection_func,
+            control_task,
+            train_test_ratio,
+        )
+
+        self.label_vocab: Vocab = corpus.fields[labels_column].vocab
+
+        assert len(self.test_split.labels) > 0, (
+            "DataSplit contains no test labels, check whether test_selection_func, "
+            "test_activation_dir or train_test_ratio are set up correctly"
+        )
+
+    @staticmethod
+    def _create_data_split(
+        activation_reader: ActivationReader,
+        corpus: Corpus,
+        labels_column: str,
+        label_ids: ActivationIndex,
+        control_task: Optional[ControlTask],
+    ) -> DataSplit:
+        """Creates a labels Tensor and returns a DataSplit.
+
+        The DataSplit contains an ActivationReader, the labels, and,
+        optionally, the control labels.
+        """
+        labels = create_labels_from_corpus(
+            corpus,
+            labels_column,
+            selection_func=activation_reader.selection_func,
+        )
+
         if control_task is not None:
-            self.train_labels_control = create_labels_from_corpus(
-                corpus, selection_func=train_selection_func, control_task=control_task
+            control_labels = create_labels_from_corpus(
+                corpus,
+                labels_column,
+                selection_func=activation_reader.selection_func,
+                control_task=control_task,
             )
-
-        self.label_vocab: Vocab = corpus.fields["labels"].tokenizer
-
-        if test_activations_dir is not None:
-            self.test_activation_reader = ActivationReader(test_activations_dir)
-            test_selection_func = test_selection_func or (
-                lambda sen_id, pos, example: True
-            )
-            assert test_corpus is not None, "`test_corpus` should be provided!"
-            self.test_labels = create_labels_from_corpus(
-                test_corpus, selection_func=test_selection_func
-            )
-            if control_task is not None:
-                self.test_labels_control = create_labels_from_corpus(
-                    test_corpus,
-                    selection_func=test_selection_func,
-                    control_task=control_task,
-                )
         else:
-            self.test_activation_reader = None
+            control_labels = None
 
-        if test_selection_func is not None:
-            self.test_labels = create_labels_from_corpus(
-                corpus, selection_func=test_selection_func
-            )
-            if control_task is not None:
-                self.test_labels_control = create_labels_from_corpus(
-                    corpus,
-                    selection_func=test_selection_func,
-                    control_task=control_task,
-                )
-
-        self.activation_reader = ActivationReader(activations_dir)
-
-        orig_selection_func = self.activation_reader.selection_func
-
-        self.train_ids, self.test_ids = self._create_train_test_mask(
-            corpus, orig_selection_func, train_selection_func, test_selection_func
+        return DataSplit(
+            activation_reader, labels[label_ids], control_labels[label_ids]
         )
 
-    def create_data_split(
+    def _create_test_split(
+        self,
+        corpus: Corpus,
+        labels_column: str,
+        train_activation_reader: ActivationReader,
+        test_activations_dir: Optional[str],
+        test_corpus: Optional[Corpus],
+        test_labels_column: Optional[str],
+        test_selection_func: Optional[SelectionFunc],
+        control_task: Optional[ControlTask],
+        train_test_ratio: Optional[float],
+    ) -> DataSplit:
+        """Creates the DataSplit for the test items.
+
+        Test items can be retrieved from a separate corpus, or from
+        a subsplit of the training corpus. In the latter case the test
+        items are either defined by a separate test_selection_func, or
+        a random
+        """
+        if test_activations_dir is not None:
+            test_activation_reader = ActivationReader(
+                test_activations_dir, cat_activations=True
+            )
+
+            self.test_ids, _ = self._train_test_ids(
+                test_corpus,
+                test_activation_reader.selection_func,
+                test_selection_func,
+                None,
+                train_test_ratio,
+            )
+
+            return self._create_data_split(
+                test_activation_reader,
+                test_corpus,
+                (test_labels_column or labels_column),
+                self.test_ids,
+                control_task,
+            )
+
+        return self._create_data_split(
+            train_activation_reader,
+            corpus,
+            labels_column,
+            self.test_ids,
+            control_task,
+        )
+
+    def load(
         self,
         activation_name: ActivationName,
-        data_subset_size: int = -1,
-        train_test_split: float = 0.9,
     ) -> DataDict:
         """Creates train/test data split of activations
 
@@ -119,96 +212,69 @@ class DataLoader:
         ----------
         activation_name : ActivationName
             (layer, name) tuple indicating the activations to be read in
-        data_subset_size : int, optional
-            Subset size of data to train on. Defaults to -1, indicating
-            the entire data set.
-        train_test_split : float
-            Percentage of the train/test split. If separate test
-            activations are provided this split won't be used.
-            Defaults to 0.9/0.1.
 
         Returns
         -------
         data_dict : DataDict
-            Dictionary mapping train and test embeddings (train_x,
-            test_x) to corpus labels (train_y, test_y), and optionally
-            to control task labels (train_y_control, test_y_control).
+            Dictionary containing train and test activations, and their
+            corresponding labels and, optionally, control labels.
         """
+        train_activations = self.train_split.activation_reader[:, activation_name]
+        test_activations = self.test_split.activation_reader[:, activation_name]
 
-        train_activations = self.activation_reader.activations(activation_name)
-
-        test_activations = train_activations[self.test_ids]
         train_activations = train_activations[self.train_ids]
+        test_activations = test_activations[self.test_ids]
 
-        if data_subset_size == -1:
-            data_size = train_activations.size(0)
-        else:
-            data_size = min(data_subset_size, train_activations.size(0))
+        train_labels = self.train_split.labels
+        test_labels = self.test_split.labels
 
-        # Shuffle activations
-        indices = np.random.choice(
-            range(train_activations.size(0)), data_size, replace=False
-        )
-
-        train_activations = train_activations[indices]
-        train_labels = self.train_labels[indices]
-        test_labels = self.test_labels
-
-        train_labels_control = self.train_labels_control
-        test_labels_control = self.test_labels_control
-        if train_labels_control is not None:
-            train_labels_control = train_labels_control[indices]
-
-        if self.test_activation_reader is not None:
-            test_activations = self.test_activation_reader.activations(activation_name)
-            test_labels = self.test_labels
-        # Create test set from split in training data
-        elif test_activations.size(0) == 0:
-            split = int(data_size * train_test_split)
-
-            test_activations = train_activations[split:]
-            test_labels = train_labels[split:]
-            train_activations = train_activations[:split]
-            train_labels = train_labels[:split]
-
-            if train_labels_control is not None:
-                test_labels_control = train_labels_control[split:]
-                train_labels_control = train_labels_control[:split]
+        train_control_labels = self.train_split.control_labels
+        test_control_labels = self.test_split.control_labels
 
         return {
-            "train_x": train_activations,
-            "train_y": train_labels,
-            "train_y_control": train_labels_control,
-            "test_x": test_activations,
-            "test_y": test_labels,
-            "test_y_control": test_labels_control,
+            "train_activations": train_activations,
+            "train_labels": train_labels,
+            "train_control_labels": train_control_labels,
+            "test_activations": test_activations,
+            "test_labels": test_labels,
+            "test_control_labels": test_control_labels,
         }
 
     @staticmethod
-    def _create_train_test_mask(
+    def _train_test_ids(
         corpus: Corpus,
         orig_selection_func: SelectionFunc,
-        train_selection_func: SelectionFunc,
+        train_selection_func: Optional[SelectionFunc],
         test_selection_func: Optional[SelectionFunc],
-    ) -> Tuple[Tensor, Tensor]:
+        train_test_ratio: Optional[float],
+    ) -> Tuple[ActivationIndex, ActivationIndex]:
         """Creates a tensor mask for the train/test split.
 
         Mask is created based on the provided selection functions.
+
         Note that the train_selection_func takes precedence over the
-        test_selection_func. The mask also takes the original
-        selection_func into account, and will skip items that are
-        not part of neither the provided selection_funcs.
+        test_selection_func.
+
+        The mask also takes the original selection_func into account,
+        and will skip items that are not part of either the provided
+        selection_funcs.
         """
         train_ids = []
         test_ids = []
 
-        for idx, item in enumerate(corpus.examples):
-            for pos in range(len(item.sen)):
-                if orig_selection_func(idx, pos, item):
-                    if train_selection_func(idx, pos, item):
-                        train_ids.append(True)
-                        test_ids.append(False)
-                    elif test_selection_func and test_selection_func(idx, pos, item):
+        for item in corpus.examples:
+            sen = getattr(item, corpus.sen_column)
+            for pos in range(len(sen)):
+                if orig_selection_func(pos, item):
+                    if train_selection_func is None or train_selection_func(pos, item):
+                        in_train = (
+                            random.random() < train_test_ratio
+                            if train_test_ratio is not None
+                            else True
+                        )
+                        train_ids.append(in_train)
+                        test_ids.append(not in_train)
+                    elif test_selection_func and test_selection_func(pos, item):
                         train_ids.append(False)
                         test_ids.append(True)
                     else:
@@ -216,6 +282,6 @@ class DataLoader:
                         test_ids.append(False)
 
         return (
-            torch.tensor(train_ids).to(torch.uint8),
-            torch.tensor(test_ids).to(torch.uint8),
+            torch.tensor(train_ids).to(torch.bool),
+            torch.tensor(test_ids).to(torch.bool),
         )
