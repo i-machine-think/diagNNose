9d3ca89b16bc23fde52c98751ff414b9344909c5
Sylke Gosen <sylkegosen@hotmail.com>
2021-04-13 18:29:57 +0300

    Update awd_lstm.py

--- diagnnose/models/wrappers/awd_lstm.py
+++ diagnnose/models/wrappers/awd_lstm.py
@@ -9,10 +9,19 @@ class AWDLSTM(ForwardLSTM):
         super().__init__(*args, **kwargs)
 
     @staticmethod
-    def param_names(layer: int, rnn_name: str, **kwargs) -> Dict[str, str]:
-        return {
-            "weight_hh": f"{rnn_name}.{layer}.module.weight_hh_l0_raw",
-            "weight_ih": f"{rnn_name}.{layer}.module.weight_ih_l0",
-            "bias_hh": f"{rnn_name}.{layer}.module.bias_hh_l0",
-            "bias_ih": f"{rnn_name}.{layer}.module.bias_ih_l0",
-        }
+    def param_names(layer: int, rnn_name: str, no_suffix: bool = False, **kwargs) -> Dict[str, str]:
+        # The AWD-LSTM has no separate weight names for a single layer LSTM
+        if no_suffix:
+            return {
+                "weight_hh": "",
+                "weight_ih": "",
+                "bias_hh": "",
+                "bias_ih": "",
+            }
+        else:
+            return {
+                "weight_hh": f"{rnn_name}.{layer}.module.weight_hh_l0_raw",
+                "weight_ih": f"{rnn_name}.{layer}.module.weight_ih_l0",
+                "bias_hh": f"{rnn_name}.{layer}.module.bias_hh_l0",
+                "bias_ih": f"{rnn_name}.{layer}.module.bias_ih_l0",
+            }
