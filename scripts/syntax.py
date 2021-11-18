8a4111e995b92febdb0d3aee7bb0a8dfa2dd2f58
jumelet <jumeletjaap@gmail.com>
2021-11-18 14:00:05 +0100

    :recycle: Update Syntactic evaluation script

--- scripts/syntax.py
+++ scripts/syntax.py
@@ -6,16 +6,16 @@ from diagnnose.config.config_dict import create_config_dict
 from diagnnose.models import LanguageModel, import_model, set_init_states
 from diagnnose.syntax.evaluator import SyntacticEvaluator
 from diagnnose.tokenizer import create_tokenizer
-from diagnnose.utils.misc import profile
+
 
 if __name__ == "__main__":
     config_dict = create_config_dict()
 
     model: LanguageModel = import_model(**config_dict["model"])
     tokenizer: PreTrainedTokenizer = create_tokenizer(**config_dict["tokenizer"])
-    set_init_states(model, use_default=True, tokenizer=tokenizer)
+    set_init_states(model, tokenizer=tokenizer, **config_dict["init_states"])
 
     suite = SyntacticEvaluator(model, tokenizer, **config_dict["downstream"])
     accuracies, scores = suite.run()
 
-    pprint(scores)
+    pprint(accuracies)
