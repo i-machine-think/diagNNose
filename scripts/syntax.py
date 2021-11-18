c26dc88fc35c9ae4aaa0cbaab3cdc2fd107de4bb
jumelet <jumeletjaap@gmail.com>
2021-11-18 18:54:05 +0100

    :recycle: Bring example scripts up to date with recent changes

--- scripts/syntax.py
+++ scripts/syntax.py
@@ -3,17 +3,16 @@ from pprint import pprint
 from transformers import PreTrainedTokenizer
 
 from diagnnose.config.config_dict import create_config_dict
-from diagnnose.models import LanguageModel, import_model, set_init_states
+from diagnnose.models import LanguageModel, import_model
 from diagnnose.syntax.evaluator import SyntacticEvaluator
 from diagnnose.tokenizer import create_tokenizer
-
 
 if __name__ == "__main__":
     config_dict = create_config_dict()
 
     model: LanguageModel = import_model(**config_dict["model"])
     tokenizer: PreTrainedTokenizer = create_tokenizer(**config_dict["tokenizer"])
-    set_init_states(model, tokenizer=tokenizer, **config_dict["init_states"])
+    model.set_init_states(tokenizer=tokenizer, **config_dict["init_states"])
 
     suite = SyntacticEvaluator(model, tokenizer, **config_dict["downstream"])
     accuracies, scores = suite.run()
